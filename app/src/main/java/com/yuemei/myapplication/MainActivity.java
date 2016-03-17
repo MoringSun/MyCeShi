d9a8925ff011dd1045cb2a2a2df2f02032008b9a
dwb <1527711606@qq.com>
2016-03-17 15:13:56 +0800

    第二次提交

--- app/src/main/java/com/yuemei/myapplication/MainActivity.java
+++ app/src/main/java/com/yuemei/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.yuemei.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tex();
     }
+
+
+    void tex(){
+        Toast.makeText(MainActivity.this,"ss",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,"aa",Toast.LENGTH_SHORT).show();
+    }
+
 }
