6a43ae1df9cdeedb9c539801d13e73b2cb54d27f
dwb <dong.wenbin@kepuchina.org.cn>
2017-06-01 15:19:44 +0800

    第一次提交测试

---
+++ app/src/main/java/com/yuemei/myapplication/CeshiDemo.java
@@ -0,0 +1,21 @@
+package com.yuemei.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by dwb on 2017/6/1.
+ */
+
+public class CeshiDemo extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Log.e("AAAAAAAAAAAAAAA","AAAAAAAAAAAAa");
+    }
+}
